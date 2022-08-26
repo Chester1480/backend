8a7d140b28c2d4447e9e44ed2e180c57ebbaeab1
chester <chester0148@gmail.com>
2022-08-26 10:35:14 +0800

    fix: 登入 功能目前暫停

--- src/routes/jobpost/company.js
+++ src/routes/jobpost/company.js
@@ -1,9 +1,114 @@
 const { mongo } = require("../../service/share/database/databasepackage");
 const { encryptJs } = require('../../service/share/lib/libpackage');
 const fluent = require('fluent-json-schema');
+
 const collectioName ='Company';
 module.exports = async function (fastify, options) {
     const i18n = fastify.i18n;
+
+    const registerSchema = {
+        body: fluent.object().prop('companyName', fluent.string().minLength(6).maxLength(40).required())
+                             .prop('account', fluent.string().minLength(6).maxLength(40).required())
+                             .prop('password', fluent.string().minLength(6).maxLength(30).required())
+                             .prop('confirmPassword', fluent.string().minLength(6).maxLength(30).required())
+                             .prop('companyDescription', fluent.string().required())
+                             .prop('mail', fluent.string().format(fluent.FORMATS.EMAIL).required()),
+    }
+
+    fastify.post('/registerCompany',{schema:registerSchema}, async (request, reply) => {
+        const { companyName , account ,password ,confirmPassword,mail,icon,companyDescription,link} = request.body;
+        const ip  = request.ip;
+
+        if(password !== confirmPassword){ //密碼不一致
+            const response = {
+                message:i18n.t('PasswordIsNotSame') ,
+                status:false,
+                data:{}
+            }
+            return reply.send(response);
+        }
+
+        const companyInfo = await mongo.findOne(collectioName, {account});
+        if(companyInfo){ //帳號已存在
+            const response = {
+                message:i18n.t('Account') +  i18n.t('Exist') ,
+                status:false,
+                data:{}
+            }
+            return reply.send(response);
+        }
+        const hashPassword = await encryptJs.bcryptHash(password);
+
+        const data = {
+            icon,//設定default
+            payOutTime:null,
+            companyName,
+            account,
+            password:hashPassword,
+            companyDescription,
+            status:1,
+            mail,
+            ip,
+            link,
+            createTime:Date.now()
+        }
+
+        const isInsertsuccess = await mongo.insert(collectioName, [data]);
+
+        let response ;
+        if(isInsertsuccess){
+            response = {
+                message:i18n.t('Register') +  i18n.t('Success') ,
+                status:false,
+                data:{}
+            }
+        }else{
+            response = {
+                message:i18n.t('Register') +  i18n.t('Fail') ,
+                status:false,
+                data:{}
+            }
+        }
+
+        return reply.send(response);
+    })
+
+    const loginSchema = {
+        body: fluent.object().prop('account', fluent.string().minLength(6).maxLength(40).required())
+                             .prop('password', fluent.string().minLength(6).maxLength(30).required())
+    }
+
+    fastify.post('/loginCompany',{schema:loginSchema}, async (request, reply) => {
+        const { account,password } = request.body;
+
+        const companyInfo = await mongo.findOne(collectioName, {account});
+
+        const hashPassword = await encryptJs.bcryptHash(password);
+        const isPass = encryptJs.bcrypCompareSync(hashPassword,companyInfo.password);
+
+        
+        if(!isPass){//密碼不正確
+            response = {
+                message:i18n.t('Register') +  i18n.t('Fail') ,
+                status:false,
+                data:{}
+            }
+            return reply.send(response);
+        }
+        
+
+        
+
+        // if(companyInfo){ //帳號已存在
+        //     const response = {
+        //         message:i18n.t('Account') +  i18n.t('Exist') ,
+        //         status:false,
+        //         data:{}
+        //     }
+        //     return reply.send(response);
+        // }
+
+    })
 
     const createSchema = {
         body: fluent.object()
@@ -27,7 +132,7 @@ module.exports = async function (fastify, options) {
             }
             return reply.send(response);
         }
-        
+
         //加密
         const hashPassword = await encryptJs.bcryptHash(password);
 
